40f902890e081ce087730c0c3a65961fafbf8e15
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-06-13 17:03:59 +0300

    Реализовала поиск ссылки книги с 10 страниц

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,21 +6,23 @@ from urllib.parse import urljoin, urlsplit, unquote
 
 
 def parse_book_category(category_url):
-    response = requests.get(category_url)
-    response.raise_for_status()
-    page_code = BeautifulSoup(response.text, "lxml")
-
-    books_urls = []
-
-    books = page_code.select("table.d_book")
-    for book in books:
-        book_id = book.find("a")["href"]
-        books_urls.append(urljoin(category_url, book_id))
-
    
 
+    books_urls = []
+    for page in range(1, 11):
+        category_url = category_url.format(page=page)
+        response = requests.get(category_url)
+        response.raise_for_status()
+        page_code = BeautifulSoup(response.text, "lxml")
+        books = page_code.select("table.d_book")
+        for book in books:
+            book_id = book.find("a")["href"]
+            books_urls.append(urljoin(category_url, book_id))
+    print(books_urls)
+    
+
 def main():
-    category_url = "https://tululu.org/l55/"
+    category_url = "https://tululu.org/l55/{page}"
     parse_book_category(category_url)
 
 
